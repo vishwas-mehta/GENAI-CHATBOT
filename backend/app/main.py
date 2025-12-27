9fed28ab90c59e123d6a133a82e70c7d49c95077
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-27 18:15:54 +0530

    Made the main.py file and the base route/

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,3 +7,16 @@ from dotenv import load_dotenv
 from app.core.config import Config
 
 load_dotenv()
+
+app=Flask(__name__)
+
+CORS(app, resources={r"/*":{"origins":"*"}})
+
+app.register_blueprint(routes_bp)
+
+app.comfig['MAX_CONTENT_LENGTH'] = Config.MAX_CONTENT_LENGTH_MB * 1024 *1024
+
+@app.route("/")
+def root():
+    return {"message": "Document Search Bot API (FLASK) is running"}
+    
