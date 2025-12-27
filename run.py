cf3484efd6628bfa9b509ddb591ce8b2ccb48e0e
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-27 13:07:32 +0530

    Made the entry point of the application

---
+++ run.py
@@ -0,0 +1,8 @@
+from app.main import app
+import os
+from app.core.config import Config
+
+if __name__ == "__main__":
+    port =Config.APP_PORT
+    app.run(host="0.0.0.0", port=port, debug = True)
+    
