9f54296d632da1b28da00e5a562a2eb8e66d285a
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-27 17:19:47 +0530

    Installed the required libraries and setup the project backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
 from flask import Flask
-form flask_cors import CORS
+from flask_cors import CORS
 from app.core.logger import setup_logging
 from app.routes import routes_bp
 import os
 from dotenv import load_dotenv
 from app.core.config import Config
+
+load_dotenv()
