690639d15cbd167a3d07880162fa130f57be3a74
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-28 12:13:06 +0530

    Made the upload route for backend

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -12,3 +12,36 @@ from app.core.chroma_store import delete_document_by_filename
 routes_bp = Blueprint("routes", __name__)
 logger = logging.getLogger(__name__)
 
+ALLOWED_EXTENTIONS = Config.ALLOWED_EXTENSIONS
+UPLOAD_FOLDER = Path(Config.UPLOAD_FOLDER)
+UPLOAD_FOLDER.mkdir(parents=True, exists_ok=True)
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENTIONS 
+
+@routes_bp.route("/upload", methods=["POST"])
+def upload_documents():
+    try:
+        if "files" not in request.files:
+            return jsonify({"error": "No files part in the request"}), 400
+        
+        files=request.files.getlist("files")
+
+        saved_files = []
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename=secure_filename(file.filename)
+                file_path=UPLOAD_FOLDER / filename
+                file.save(file_path)
+                saved_files.append(str(file_path))
+            else:
+                return jsonify({"error": f"Unsupported file type: {file.filename}"}), 400
+        
+        texts = extract_texts_from_filepaths(saved_files)
+        if not texts:
+            raise Exception(status_code=400, detail="No vaild documents to process.")
+
+        process_documents(texts)
+        return jsonify(content = {"message" : "Documents uploaded and indexed successfully."})
+    except Exception as e:
+                    
