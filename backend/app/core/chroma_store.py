24fea8d4ffd425e662364f21505205e124f09294
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-28 17:01:45 +0530

    Made the query_similar_document function in chroma_store file

--- backend/app/core/chroma_store.py
+++ backend/app/core/chroma_store.py
@@ -19,3 +19,19 @@ def add_documents_to_vector_store(docs: list[str], ids: list[str], filename: str
         raise ValueError(f"Failed to add documents to vector store: {e}")
 
         
+def query_similar_document(query: str, top_k: int = 3) -> list[str]:
+    try:
+        context_files = get_uploaded_files()
+        print(f"Context files available: {context_files}")
+        if len(context_files) == 0:
+            return []
+
+        query_vector = get_embedding(query)
+        results = collection.query(query_embedding = [query_vector], n_results= tok_k, where = {"filename": {"$in": context_files}})
+
+        return results["documents"][0]
+    except Exception as e:
+        logger.error(f"Error querying similar documents: {e}")
+        raise ValueError(f"failed to uery similar documents: {e}")
+        
+                        
