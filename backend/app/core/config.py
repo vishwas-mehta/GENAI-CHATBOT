5e7a193bf12a2753db8ad5ec5105dc1d49238a1d
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-29 11:27:55 +0530

    Made the config file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,4 +6,8 @@ load_dotenv()
 class Config:
     APP_PORT = int(os.getenv("APP_PORT", 8000))
     ALLOWED_EXTENSIONS = {"pdf", "docx", "pptx", "xlsx"}
-    MAX_CONTENT_LENGTH_MB = int(os.getenv("MAX_CONTENT_LENGTH_MB", 5))
+    UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "data/uploads")
+    LOG_DIR = os.getenv("LOG_DIR", "logs")
+    VECTOR_DIR = os.getenv("VECTOR_DIR", "data/vectorstore")
+    MAX_CONTEXT_LENGTH_MB = int(os.getenv("MAX_CONTENT_LENGTH_MB", 2))
+    
