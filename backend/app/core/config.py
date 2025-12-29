f0ea2cf3080ebabec3e75b43a763528160ffc564
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-29 11:23:19 +0530

    Made the config class

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    APP_PORT = int(os.getenv("APP_PORT", 8000))
+    ALLOWED_EXTENSIONS = {"pdf", "docx", "pptx", "xlsx"}
+    MAX_CONTENT_LENGTH_MB = int(os.getenv("MAX_CONTENT_LENGTH_MB", 5))
