5b88e062d7284ea0dd72f41578dcacadf286f281
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-29 11:38:40 +0530

    Made the extract text from pdf function

--- backend/app/core/doc_loader.py
+++ backend/app/core/doc_loader.py
@@ -8,3 +8,15 @@ import logging
 
 from app.config import Config
 from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+SUPPORTED_EXTENTIONS = Config.ALLOWED_EXTENSIONS
+
+def extract_text_from_pdf(file_path: str) -> str:
+    doc= fitz.open(file_path)
+    text=""
+    for page in doc:
+        text+=page.get_text()
+    return text
+
