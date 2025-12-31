f09b26b13ad988c7a9399a647fbb9e9ebaf09476
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-31 15:21:38 +0530

    Did the necessary imports

--- backend/app/core/llm_query.py
+++ backend/app/core/llm_query.py
@@ -5,3 +5,8 @@ from typing import List
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import re
+import chromadb
+from chromadb.config import Settings
+from app.core.chroma_store import query_similar_documents, add_documents_to_vector_store
+
+from huggingface_hub import InferenceClient
