dd13e4cc7e77d2be6582f6cf4533167cdd3f0287
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-31 15:06:36 +0530

    Loaded the headers to be passed with requests

--- backend/app/core/embedding.py
+++ backend/app/core/embedding.py
@@ -2,3 +2,8 @@ import requests
 from typing import List 
 import os
 import logging  
+
+headers = {
+    "Authorization": f"Bearer {os.getenv("HUGGINGFACEHUB_API_TOKEN")}",
+    "Content-Type": "application/json"
+}
