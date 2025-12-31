1de1c84b38c8b6d4c7b5100f515cc49eb9cdba55
vishwas-mehta <vishwas.mehta.work@gmail.com>
2025-12-31 15:13:00 +0530

    Made the get embedding function

--- backend/app/core/embedding.py
+++ backend/app/core/embedding.py
@@ -10,3 +10,18 @@ headers = {
 
 logger = logging.getLogger(__name__)
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "sentence-transformers/distilbert-base-nli-mean-tokens")
+
+
+def get_embedding(text: str) -> List[float]:
+    try:
+        url = f"https://api-inference.huggingface.co/models/{EMBEDDING_MODEL}"
+        print(f"url: {url}")
+        response = request.post(url, headers, json = {"inputs": text}, timeout=60, veryify=False)
+        response.raise_for_status()
+        embedding = response.json()
+        return embedding
+
+    except Exception as e:
+        logger.exception("Error getting embedding")
+        raise ValueError("Failed to get embedding from model.")
+         
